cefcb360e5a5f03992c47475a9215d83cb8ee8da
Thant Ko Zaw <thantmkou10@gmail.com>
2024-05-14 16:42:44 +0630

    changes

---
+++ omit.ts
@@ -0,0 +1,49 @@
+type DotPrefix<T extends string> = T extends "" ? "" : `.${T}`;
+
+type DotNestedKeys<T> = T extends object
+  ? {
+      [K in Exclude<keyof T, symbol>]: `${K}${"" | `.${DotNestedKeys<T[K]>}`}`;
+    }[Exclude<keyof T, symbol>]
+  : never;
+
+type User = {
+  name: string;
+  preferences: {
+    gender: string;
+    sexuality: string;
+  };
+  age: number;
+};
+
+type UserKeys = DotNestedKeys<User>;
+
+type UnDotToObj<
+  Obj,
+  Keys extends DotNestedKeys<Obj>
+> = Keys extends `${infer First}.${infer Rest}`
+  ? {
+      [K in keyof Obj extends First ? never : First]: `${First}.${Rest}`;
+    }
+  : {
+      [K in keyof Obj]: Obj[K];
+    };
+
+const test: UnDotToObj<User, "preferences.gender"> = {};
+
+type Test = {
+  user: {
+    name: string;
+  };
+};
+
+type OneKey = "user.name";
+
+type Structured = OneKey extends `${infer User}.${infer Name}`
+  ? {
+      [K in keyof Test extends User ? "user" : never]: Test[K] extends object
+        ? {
+            [L in keyof Test[K] extends Name ? "name" : never]: Test[K][L];
+          }
+        : never;
+    }
+  : never;
